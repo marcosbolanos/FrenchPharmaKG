4359d019999de9c65c1402b8d4f6a8e895cf748a
marcosbolanos <marcosqbv@gmail.com>
2025-05-24 19:26:57 +0200

    updated init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,10 @@
--- init.sql
-CREATE EXTENSION age;
+-- Create the target database
+CREATE DATABASE fpkg;
+
+-- Connect to that database and set up extensions
+\connect fpkg;
+
+CREATE EXTENSION IF NOT EXISTS age;
 CREATE EXTENSION IF NOT EXISTS vector;
+
 SET search_path = ag_catalog, pg_catalog;
